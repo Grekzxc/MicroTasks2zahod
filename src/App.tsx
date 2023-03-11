3323510d037a582c43de2355025812f2fb77ef14
Grekzxc <cool.grek456852@yandex.by>
2023-03-11 22:07:54 +0300

    useState2

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,21 @@ import { NewComponent, NewComponent2, topCars } from './site/NewComponent';
 
 function App() {
 
-  let a = 1
+  let [a, setA] = useState(1)
 
   const onClickHandler = () => {
-    ++a
+    setA(++a)
     console.log(a);
-
+  }
+  const onClickHandler1 = () => {
+    setA(0)
   }
 
   return (
     <div className='App'>
       <h1>{a}</h1>
       <button onClick={onClickHandler}>x</button>
+      <button onClick={onClickHandler1}>0</button>
     </div>
   );
 }
